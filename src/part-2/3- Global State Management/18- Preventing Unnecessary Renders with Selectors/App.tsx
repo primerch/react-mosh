1eb5b9b2be7863a6c15ad4bd52070df78e51d452
Reacher <macryze@icloud.com>
2025-09-06 01:39:11 +0800

    feat(part-2): add App component for Preventing Unnecessary Renders with Selectors
    - Create App.tsx file
    - Import and render components A and B

---
+++ src/part-2/3- Global State Management/18- Preventing Unnecessary Renders with Selectors/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import A from './part-2/3- Global State Management/18- Preventing Unnecessary Renders with Selectors/A';
+import B from './part-2/3- Global State Management/18- Preventing Unnecessary Renders with Selectors/B';
+
+function App() {
+  return (
+    <>
+      <A />
+      <B />
+    </>
+  );
+}
+
+export default App;
